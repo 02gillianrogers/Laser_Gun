79f13af3e3d0555274c6c9b25abacf8e62869f3e
acecool11 <44272139+acecool11@users.noreply.github.com>
2019-01-08 11:09:20 -0800

    Add files via upload

---
+++ sketch_jan08a/sketch_jan08a.ino
@@ -0,0 +1,42 @@
+int Laser = 6;
+int Detector = 7;
+
+int x = 0;
+
+void setup()
+{ 
+  Serial.begin (9600);
+  pinMode(Laser, OUTPUT);
+  pinMode(Detector, INPUT);
+ 
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(10, OUTPUT);
+  
+}
+
+void loop()
+{
+  digitalWrite(Laser, HIGH);
+  boolean val = digitalRead(Detector);
+  Serial.println(val);
+}  
+
+void loop()
+{
+  
+  digitalWrite(3, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(10, HIGH);
+  while (analogRead(A0) == 310) {
+    digitalWrite(3, LOW);
+    delay(1000); // Wait for 1000 millisecond(s)
+    while (analogRead(A0) == 310) {
+      digitalWrite(5, LOW);
+      delay(1000); // Wait for 1000 millisecond(s)
+      while (analogRead(A0) == 310) {
+        digitalWrite(10, LOW);
+      }
+    }
+  }
+}
